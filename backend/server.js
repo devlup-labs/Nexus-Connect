595c671b0eb7503a78a332d83f77905ccc111b5a
beepboop876 <divyanshmalik233@gmail.com>
2026-01-11 14:39:26 +0530

    backend- authentication (login, logout, singup, update profile)

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { connectDB } from "./src/lib/db.js";
+import authRoutes from "./src/routes/auth.route.js";
+
+
 dotenv.config();
 
 const app = express();
@@ -9,9 +16,20 @@ const _dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.get("/health", (req, res) => {  //DO NOT REMOVE THIS ENDPOINT
   res.status(200).json({ msg: "api is up and running" });
 });
+
+app.use("/api/auth", authRoutes);
 
 //This is for deploying the frontend
 if (process.env.NODE_ENV === 'production') {
@@ -24,4 +42,5 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
