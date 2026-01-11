f2c1dc3de1c6cf149ed6693e56e74bc6b9794e25
Tanmay-Bagadia <tanmaybagadia13@gmail.com>
2026-01-11 17:22:47 +0530

    Database connection and resend config done

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const _dirname = path.resolve();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/health", (req, res) => {
+  //DO NOT REMOVE THIS ENDPOINT
+  res.status(200).json({ msg: "api is up and running" });
+});
+
+//This is for deploying the frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
