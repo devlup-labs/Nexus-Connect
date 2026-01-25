fefed2055bd2d6fad382a54bcafb86ae9a43177f
Tanmay-Bagadia <tanmaybagadia13@gmail.com>
2026-01-25 22:03:27 +0530

    Basic Settins Panel done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import Dock from './Components/Dock.jsx'
 import StreamPanel from './Components/StreamPanel.jsx'
+import SettingsPanel from './Components/Settings.jsx'
 
 function App() {
 
 
   return (
     <>
-      <Dock/>
-      <StreamPanel/>
+
+      <Dock />
+      <StreamPanel />
     </>
   )
 }
